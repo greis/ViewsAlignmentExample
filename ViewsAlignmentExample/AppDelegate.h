d84375f37d6ff78600d2b0dafc989a86d2e717ab
Gabriel Reis <gabriel.oreis@gmail.com>
2013-09-26 14:56:13 -0400

    Initial Commit

---
+++ ViewsAlignmentExample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ViewsAlignmentExample
+//
+//  Created by Gabriel Reis on 9/26/13.
+//  Copyright (c) 2013 Gabriel Reis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ViewsAlignmentExample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ViewsAlignmentExample
+//
+//  Created by Gabriel Reis on 9/26/13.
+//  Copyright (c) 2013 Gabriel Reis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
